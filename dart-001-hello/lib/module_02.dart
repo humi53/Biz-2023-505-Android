617702623f5b245ca79952582bb5bda59042d105
humi53 <j.k.humi9@gmail.com>
2023-08-30 15:42:59 +0900

    화면구성 배우기

--- dart-001-hello/lib/module_02.dart
+++ dart-001-hello/lib/module_02.dart
@@ -1,7 +1,8 @@
 String sayHello(String? name, int? age, String? nation) =>
     "안녕 $name 야, 당신의 나이는 $age 이고, 고향은 $nation 이야!!";
 
-String sayHelloNameParams({String? name, int? age, String? nation}) =>
+String sayHelloNameParams(String contry,
+        {String? name, int? age, String? nation}) =>
     "안녕 $name 야, 당신의 나이는 $age 이고, 고향은 $nation 이야!!";
 
 
