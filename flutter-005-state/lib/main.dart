beed65ae297db7afd2804bafeebfa8bd5a65b1dc
humi53 <j.k.humi9@gmail.com>
2023-08-31 15:52:16 +0900

    리스트&탐색

--- flutter-005-state/lib/main.dart
+++ flutter-005-state/lib/main.dart
@@ -58,17 +58,19 @@ class StartPage extends State<HomePage> {
     Student(stNum: "006", stName: "장녹수"),
   ];
 
+  List<Student> filterList = [];
+
   /// 동적으로 변화되는 배열(리스트) 요소들을 화면에 출력하기 위하여
   /// ListView.builder() 함수를 사용하여 각 요소를 디자인한다
   ListView appBarBody() => ListView.builder(
-        itemCount: studentList.length,
+        itemCount: filterList.length,
         itemBuilder: (context, index) {
           return ListTile(
             title: Material(
               child: InkWell(
                 onTap: () {
                   var snackBar = SnackBar(
-                    content: Text(studentList[index].stName),
+                    content: Text(filterList[index].stName),
                   );
                   ScaffoldMessenger.of(context).showSnackBar(snackBar);
                 },
@@ -80,8 +82,8 @@ class StartPage extends State<HomePage> {
                   padding: const EdgeInsets.all(20.0),
                   child: Row(
                     children: [
-                      Text("${studentList[index].stNum} : "),
-                      Text(studentList[index].stName),
+                      Text("${filterList[index].stNum} : "),
+                      Text(filterList[index].stName),
                     ],
                   ),
                 ),
@@ -90,6 +92,34 @@ class StartPage extends State<HomePage> {
           );
         },
       );
+
+  void _onChangeHandler(String search) {
+    List<Student> result = [];
+    if (search.isNotEmpty) {
+      result =
+          studentList.where((item) => item.stName.contains(search)).toList();
+    } else {
+      result = studentList;
+    }
+    setState(() {
+      filterList = result;
+    });
+  }
+
+  /// state 가 최초에 mount 될때
+  @override
+  void initState() {
+    filterList = studentList;
+    super.initState();
+  }
+
+  /// state 가 unmount 될때
+  /// 일부 context 에 저장된 변수들을 사용 해제 해야할 경우가 있는데
+  /// 이때 여기에 그러한 코드를 작성한다
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -114,7 +144,38 @@ class StartPage extends State<HomePage> {
           )
         ],
       ), // mainAppBar(context),
-      body: appBarBody(),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) => _onChangeHandler(value),
+              decoration: const InputDecoration(
+                labelText: "Search",
+                labelStyle: TextStyle(fontSize: 20),
+                hintText: "검색어를 입력하세요",
+                hintStyle: TextStyle(color: Colors.blue),
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            // ListView 를 사용하여 list 보이기
+            // Expanded 실행하여 Column box 에 가득차게 구현
+            Expanded(
+              child: filterList.isNotEmpty
+                  ? appBarBody()
+                  : const Text(
+                      "찾는 값이 없음",
+                      style: TextStyle(fontSize: 25),
+                    ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ flutter-005-state/lib/main_app_bar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+AppBar mainAppBar(BuildContext context) => AppBar(
+      title: const Text(
+        "안녕하세요",
+        // style: TextStyle(
+        //   color: Colors.green,
+        // ),
+      ),
+      // backgroundColor: Colors.white,
+      centerTitle: true,
+      elevation: 5.0,
+      actions: [
+        IconButton(
+          onPressed: () {
+            const snackBar = SnackBar(content: Text("안녕하세요"));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          },
+          icon: const Icon(Icons.person),
+        ),
+      ],
+    );
